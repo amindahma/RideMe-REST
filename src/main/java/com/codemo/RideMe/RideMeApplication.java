cf18610cadee95c1eaac4abfb747a16a5275bcb5
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-09-13 22:46:34 +0530

    init

---
+++ src/main/java/com/codemo/RideMe/RideMeApplication.java
@@ -0,0 +1,12 @@
+package com.codemo.RideMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideMeApplication.class, args);
+	}
+}
